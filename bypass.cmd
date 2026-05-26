84ea0352f5abf457f717c1d248b0468dc8062842
mousman33 <192235173+mousman33@users.noreply.github.com>
2026-05-26 15:19:01 -0500

    moved download of install.ps1 file to the initial bypass.cmd file before sysprep.

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,4 @@
+mkdir c:\temp
+curl -L https://github.com/mousman33/bypassnro/raw/refs/heads/main/Install.ps1 -o c:\temp\install.ps1
 curl -L -o C:\Windows\Panther\unattend.xml https://github.com/mousman33/bypassnro/raw/refs/heads/main/unattend.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
