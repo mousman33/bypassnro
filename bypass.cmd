965d6d4180df540c1440f570b1779175efb97b04
mousman33 <192235173+mousman33@users.noreply.github.com>
2026-06-07 21:05:53 -0500

    Refactor bypass.cmd to improve script documentation and maintainability

--- bypass.cmd
+++ bypass.cmd
@@ -1,4 +1,15 @@
+@echo off
+:: ========================================================================================
+:: BypassNRO - A simple script to bypass the OOBE and automate first time setup on Windows 11.
+:: NOTE: Running 'sysprep.exe /oobe' without '/generalize' preserves OEM drivers, but it
+:: skips the XML 'specialize' pass and thus the scripts that run during that phase.
+:: WORKAROUND: This script pre-downloads install.ps1 and inserts the regedit to run it 
+:: after sysprep and first logon.
+:: ========================================================================================
+
+
 mkdir c:\temp
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "BypassNRO" /t REG_SZ /d "powershell -ExecutionPolicy Bypass -File C:\temp\install.ps1" /f
 curl -L https://github.com/mousman33/bypassnro/raw/refs/heads/main/Install.ps1 -o c:\temp\install.ps1
 curl -L -o C:\Windows\Panther\unattend.xml https://github.com/mousman33/bypassnro/raw/refs/heads/main/unattend.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
